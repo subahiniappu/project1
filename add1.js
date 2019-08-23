1004dd30a117d92f0c394561a56c20183bfb3c12
subahiniappu <47855007+subahiniappu@users.noreply.github.com>
2019-08-23 14:23:57 +0530

    Add files via upload

---
+++ add1.js
@@ -0,0 +1,5 @@
+var addition1=require('./addition1.js');
+console.log(addition1.addnumber(1,2));
+console.log(addition1.subnumber(22,44));
+console.log(addition1.multinumber(21,32));
+console.log(addition1.divnumber(10,5));
