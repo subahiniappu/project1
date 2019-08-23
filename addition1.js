fca3947fd16d3bf033a4a461cd828d9271f3ccfe
subahiniappu <47855007+subahiniappu@users.noreply.github.com>
2019-08-23 14:22:51 +0530

    Add files via upload

---
+++ addition1.js
@@ -0,0 +1,17 @@
+var exports=module.exports={};
+exports.addnumber=function(a,b)
+{
+return a+b;
+}
+exports.subnumber=function(a,b)
+{
+return a-b;
+}
+exports.multinumber=function(a,b)
+{
+return a*b;
+}
+exports.divnumber=function(a,b)
+{
+return a/b;
+}
